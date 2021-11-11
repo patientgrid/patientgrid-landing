6dcf3495d5ef2d1aedf311e61f2dc61f7fb1819e
Alex Recarey <alex@recarey.org>
2021-11-11 22:45:46 +0100

    working on privacy page

--- src/templates/legalTemplate.js
+++ src/templates/legalTemplate.js
@@ -1,11 +1,14 @@
 import * as React from "react"
-
+import Navigation from '../components/navigation'
+import Footer from '../components/footer'
 
 export default function Privacy() {
   return (
     <div className="bg-white">
       <div className="relative overflow-hidden">
-        
+        <Navigation/>
+
+        <Footer/>
       </div>
     </div>)
 }
