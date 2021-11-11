b28b8a611c6b7155d5eb07e22c82a06181607da7
Alejandro Recarey <alex@recarey.org>
2021-11-11 15:39:11 +0100

    Updated markdown and organization of markdown

---
+++ src/templates/legalTemplate.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+
+export default function Privacy() {
+  return (
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+        
+      </div>
+    </div>)
+}
