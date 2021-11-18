91573512f42a96eb32274a4bd60a474fdb0606f6
Alejandro Recarey <alex@recarey.org>
2021-11-18 16:37:20 +0100

    Fixed landing form

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,6 +131,7 @@ export default function Landing() {
                             <input
                               id="email"
                               type="email"
+                              name="email"
                               placeholder="Enter your email"
                               className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
                             />
