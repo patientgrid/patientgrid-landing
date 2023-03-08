db5834a4cf3712325188db266a7141faf2c42811
Alex Recarey <alex@recarey.org>
2023-03-08 16:40:12 +0100

    Changed to barebones page made by draftss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
-const colors = require('tailwindcss/colors')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./public/*.html"
+  ],
   theme: {
     extend: {
       colors: {
-        teal: colors.teal,
-        cyan: colors.cyan,
+        nav: {
+          100: "rgba(217, 217, 217, 0.5)",
+          200: "#2A0896",
+          300: "#F5F5F5",
+          400: "rgba(40, 40, 40, 0.7)",
+          500: "rgba(72, 209, 204, 0.37)",
+          700: "#D9DEEF",
+          800: "#4482C2"
+        }
       },
+
+      textColor: {
+        para: "rgba(40, 40, 40, 0.7)",
+        para2: "rgba(0, 0, 0, 0.7)"
+      },
+
+      fontFamily: {
+        poppins: ['Poppins']
+      }
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
